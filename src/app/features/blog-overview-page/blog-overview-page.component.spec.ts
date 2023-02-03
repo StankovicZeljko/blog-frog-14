f82fb7a4ec0fd2f7bae591ab87ecf929ab9c1586
Roger Rudin <roger.rudin@mobi.ch>
2023-02-03 16:02:22 +0100

    feat(): basic tests added

--- src/app/features/blog-overview-page/blog-overview-page.component.spec.ts
+++ src/app/features/blog-overview-page/blog-overview-page.component.spec.ts
@@ -1,5 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MockProvider } from 'ng-mocks';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockComponent, MockDirective, MockProvider, ngMocks } from 'ng-mocks';
+import { BlogOverviewCardComponent } from '../../shared/blog-overview-card/blog-overview-card.component';
+import { ResponsiveGridColumnsDirective } from '../../shared/responsive-grid-columns/responsive-grid-columns.directive';
 
 import { BlogOverviewPageComponent } from './blog-overview-page.component';
 import { BlogService } from './services/blog.service';
@@ -8,20 +15,74 @@ import { BlogOverviewState } from './state/blog-overview-state';
 describe('BlogOverviewPageComponent', () => {
   let component: BlogOverviewPageComponent;
   let fixture: ComponentFixture<BlogOverviewPageComponent>;
+  let blogOverviewState: BlogOverviewState;
+  let router: Router;
+  let blogService: BlogService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BlogOverviewPageComponent ],
-      providers: [MockProvider(BlogService), MockProvider(BlogOverviewState)]
+      declarations: [BlogOverviewPageComponent,
+        MockComponent(MatProgressSpinner),
+        MockComponent(BlogOverviewCardComponent),
+        MockDirective(ResponsiveGridColumnsDirective)],
+      providers: [
+        MockProvider(BlogService, { loadBlogs: jasmine.createSpy('loadBlogs')}),
+        BlogOverviewState,
+        MockProvider(Router, {
+          navigate: jasmine.createSpy('navigate').and.returnValue(Promise.resolve(true))
+        })
+      ],
+      imports: [
+        CommonModule,
+        RouterTestingModule,
+        MatGridListModule
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(BlogOverviewPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    blogOverviewState = TestBed.inject(BlogOverviewState);
+    router = TestBed.inject(Router);
+    blogService = TestBed.inject(BlogService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show spinner on loading', () => {
+
+    blogOverviewState.setState({ loading: true });
+    fixture.detectChanges();
+
+    const matSpinnerElement = ngMocks.find(MatProgressSpinner);
+    expect(matSpinnerElement).not.toBeNull();
+  });
+
+  it('should load Blogs on create', () => {
+    expect(blogService.loadBlogs).toHaveBeenCalled();
+  });
+
+  it('should navigate to detail if selectBlog emits', () => {
+
+    blogOverviewState.setState({ loading: false, blogs: [{ id: 1, title: 'a title'}]} as any);
+    fixture.detectChanges();
+
+    const blogOverviewCardComponent = ngMocks.find(BlogOverviewCardComponent).componentInstance;
+    blogOverviewCardComponent.selectBlog$.emit(1);
+
+    expect(router.navigate).toHaveBeenCalledWith(['/detail/', 1]);
+  });
+
 });
+
+
+// 1. OK should show spinner on loading
+// 2. should show error on error
+// 3. should render BlogOverviewCardComponent
+// 4. OK should navigate to detail if selectBlog emits
+// 5. should call put likeBlog if likeBlog emits
+// 6. should load Blogs on create
--- src/app/shared/blog-overview-card/blog-overview-card.component.spec.ts
+++ src/app/shared/blog-overview-card/blog-overview-card.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { runOnPushChangeDetection } from '../../testing/test-utils';
 
 import { BlogOverviewCardComponent } from './blog-overview-card.component';
 
@@ -23,4 +25,66 @@ describe('BlogOverviewCardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('it should display the title of the blog', () => {
+    const titleElement = fixture.debugElement.query(By.css('[data-testid="title"]'));
+    expect(titleElement.nativeElement.textContent).toContain('title');
+  });
+
+  it('should show the like button in black if the blog was not liked', () => {
+
+    component.model = {
+      author: 'author',
+      title: 'title',
+      id: 1,
+      likedByMe: false,
+    };
+
+    runOnPushChangeDetection(fixture);
+
+    const likeButton = fixture.debugElement.query(By.css('[data-testid="like-button-icon"]'));
+    expect(likeButton.nativeElement.style.color).toBe('black');
+  });
+
+  it('should show the like button in red if the blog was liked', async () => {
+    component.model = {
+      id: 1,
+      author: 'a author',
+      likedByMe: true,
+      title: 'A title'
+    };
+    runOnPushChangeDetection(fixture);
+
+    const likeButton = fixture.debugElement.query(By.css('[data-testid="like-button-icon"]'));
+    expect(likeButton.nativeElement.style.color).toBe('red');
+  });
+
+  it('should emit likeBlog$ if like Button was clicked', () => {
+
+    let currentEvent: any;
+    component.likeBlog$.subscribe(event => {
+      currentEvent = event;
+    });
+
+    const likeButton = fixture.debugElement.query(
+      By.css('[data-testid="like-button"]')
+    );
+
+    likeButton.triggerEventHandler('click', {});
+
+    expect(currentEvent).toEqual({ id: 1, likedByMe: true });
+
+  });
+
+
+  // 1. OK it should display the title of the blog
+  // 2. it should display the author on subtitle of the blog
+  // 3. it should display an image
+  // 4. it should have a alt image tag
+  // 5. it should emit selectBlog$ on header click or image
+  // 6. should show a like Button
+  // 7. OK should show the like Button in red if likedByMe is true
+  // 8. OK should emit likeBlog$ if like Button was clicked
+  // 9. OK should show the like button in black if the blog was not liked
+
 });
